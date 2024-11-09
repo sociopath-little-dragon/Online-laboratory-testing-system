60a9aa593c3f49018d8612a770a82115b049e0a9
sociopath-little-dragon <ilgam.shaidullin@gmail.com>
2024-11-09 23:16:53 +0300

    New config file and some changes assciated with this

--- sdo-react-main/back/uvicorn_wrapper.py
+++ sdo-react-main/back/uvicorn_wrapper.py
@@ -4,6 +4,8 @@ from modules.configs.config_funcs import load_config
 
 config = load_config()
 
-os.environ["DB_CON"] = "sqlite:///database/tst.db"
+# os.environ["DB_CON"] = "sqlite:///database/tst.db?check_same_thread=False"
+os.environ["DB_CON"] = "sqlite:///sdo-react-main/back/database/tst.db?check_same_thread=False"
+# os.environ["DB_CON"] = "sqlite:///tst.db?check_same_thread=False"
 from main import app
 uvicorn.run(app, host=config["uvicorn"]["host"], port=config["uvicorn"]["port"])
